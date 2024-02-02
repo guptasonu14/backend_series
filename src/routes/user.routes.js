c1825213b85286b369a37a72ad7e3afd6d7e5f06
guptasonu14 <sguptabudha@gmail.com>
2024-02-03 00:24:54 +0530

    add registered controller and bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,23 @@
 import {Router} from "express";
-import { registerUser } from "../controllers/user.controller";
-
+import { registerUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
 const router = Router()
 
 
-router.route("./register").post(registerUser)
+router.route("/register").post(
+    //upload is middle ware 
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            nam: "coverImage",
+            maxCount:1
+        }
+    ]),
+    
+    registerUser)
 
 
 export default router;
